b42e3bd4b092d852e697d6999ec7d2658cd92523
far_faqiri <farida.faqiri2@gmail.com>
2024-09-03 17:10:23 +0430

    changed the name of my files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-
 import App from './App';
-import './index.css'; // Ensure your Tailwind CSS is imported
+import store from './redux/store';
+import './index.css'; // Ensure Tailwind CSS is imported
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById('root');
 
-);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+} else {
+  console.error('Root element not found');
+}
