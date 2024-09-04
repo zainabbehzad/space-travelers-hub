86048ac851ab9fee8ddadb46267386e42d0d9597
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-09-05 01:19:21 +0430

    Added the Dragons file with it is neccessary codes

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -8,7 +8,7 @@ const MyProfile = () => {
   const filterMissions = missionData.filter((mission) => mission.reserved);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-[100px]"> {/* Updated margin */}
       <div className="mission-card">
         <h2 className="text-xl font-bold mb-4">My Missions</h2>
         <ul className="profile-card border border-gray-800 rounded-lg p-4">
